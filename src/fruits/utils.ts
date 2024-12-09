da5e8449cc23c86cc9582e61e2cefd265756c05c
Emil Johansson <Emilaik@gmail.com>
2024-12-09 07:55:20 +0100

    quest-II_done

--- src/fruits/utils.ts
+++ src/fruits/utils.ts
@@ -1,41 +1,73 @@
-export const getFruit = (name) => {
-	// använd fruits-arrayen och returnera det fruit-objekt som matchar name
-	// finns det ingen sådan fruit så returnera undefined
+
+import { fruits } from "./fruits";
+
+// använd fruits-arrayen och returnera det fruit-objekt som matchar name
+// finns det ingen sådan fruit så returnera undefined
+export const getFruit = (name: string) => {
+    return fruits.find(fruit => fruit.name === name);
 };
 
-export const getIcon = (name) => {
-	// använd fruits-arrayen och returnera ikonen som matchar frukten name
-	// finns det ingen sådan frukt så returnera undefined
+// använd fruits-arrayen och returnera ikonen som matchar frukten name
+// finns det ingen sådan frukt så returnera undefined
+export const getIcon = (name: string) => {
+    const fruit = getFruit(name);
+    return fruit ? fruit.icon : undefined;
 };
 
-export const getColor = (name) => {
-	// använd fruits-arrayen och returnera färgen som matchar frukten name
-	// finns det ingen sådan frukt så returnera undefined
+
+// använd fruits-arrayen och returnera färgen som matchar frukten name
+// finns det ingen sådan frukt så returnera undefined
+export const getColor = (name: string) => {
+	const fruit = getFruit(name);
+	return fruit ? fruit.color : undefined;
 };
 
 // gör en till funktion som man ger ett fruktnamn och som returnerar en frukts kategori
+export const getCategories = (name: string) => {
+	const fruit = getFruit(name);
+	return fruit ? fruit.category : undefined;
+};
 
-export const numberOfColoredFruits = (color) => {
-	// använd fruits-arrayen och returnera hur många frukter det finns av en given färg color
-	// finns det inga frukter av den färgen så svara 0
+// använd fruits-arrayen och returnera hur många frukter det finns av en given färg color
+// finns det inga frukter av den färgen så svara 0
+export const numberOfColoredFruits = (color: string) => {
+	return fruits.filter(fruit => fruit.color === color).length;
 };
 
 // gör en numberOfCategoryFruits här också
 // använd fruits-arrayen och returnera hur många frukter det finns av en given kategori
+export const numberOfCategoryFruits =(category: string) => {
+	return fruits.filter(fruit => fruit.category === category).length;
+};
 
-export const getFruitsByCategory = (category) => {
-	// använd fruits-arrayen
-	// ta emot en kategori och returnera en array av alla frukter som ingår i den kategorin
+// använd fruits-arrayen
+// ta emot en kategori och returnera en array av alla frukter som ingår i den kategorin
+export const getFruitsByCategory = (category: string) => {
+	return fruits.filter(fruit => fruit.category === category);
 };
 
 // gör en getFruitsByColor också
+export const getFruitsByColor = (color:string) => {
+	return fruits.filter(fruit => fruit.color === color);
+};
 
-export const getIconsByCategory = (category) => {
-	// använd fruits-arrayen
-	// ta emot en kategori och returnera alla ikoner från alla frukter i den kategorin
+// använd fruits-arrayen
+// ta emot en kategori och returnera alla ikoner från alla frukter i den kategorin
+export const getIconsByCategory = (category:string) => {
+	return fruits.filter(fruit => fruit.category === category).map(fruit => fruit.icon);
 };
 
 // gör en getIconsByColor också
-
+export const getIconsByColor = (color: string) => {
+    return fruits
+        .filter(fruit => fruit.color === color)
+        .map(fruit => fruit.icon);
+};
 // gör en getCategories som returnerar en array av alla kategorier
+export const getCategorie = () => {
+    return Array.from(new Set(fruits.map(fruit => fruit.category)));
+};
 // gör en getColors som returnerar en array av alla färger
+export const getColors = () => {
+    return Array.from(new Set(fruits.map(fruit => fruit.color)));
+};
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,51 @@
 import "./styles.css";
+import "./fruits/fruits";
+import { getCategorie, getCategories, getColor, getColors, getFruit, getFruitsByCategory, getFruitsByColor, getIcon, getIconsByCategory, getIconsByColor, numberOfCategoryFruits, numberOfColoredFruits } from "./fruits/utils";
+
 
 console.log("Welcome to fruit-types-quest-II, please fix the code!");
 
 // skriv ett program här nedan som testar alla de funktioner som finns i fruits/utils
+// console.log(getFruit("grape")); 
+// console.log(getIcon("banana")); 
+// console.log(getColor("lemon")); 
+// console.log(getCategories("olive")); 
+// console.log(numberOfColoredFruits("green")); 
+// console.log(numberOfCategoryFruits("citrus")); 
+// console.log(getFruitsByCategory("berry")); 
+// console.log(getFruitsByColor("red")); 
+// console.log(getIconsByCategory("berry")); 
+// console.log(getIconsByColor("yellow")); 
+// console.log(getCategorie()); 
+// console.log(getColors()); 
+
+// console.log(getFruit("grape")); 
+// console.log(getFruit("nonexistent")); 
+// console.log(getIcon("banana")); 
+// console.log(getIcon("nonexistent")); 
+// console.log(getColor("lemon")); 
+// console.log(getColor("nonexistent")); 
+// console.log(getCategorie()); 
+// console.log(numberOfColoredFruits("green")); 
+// console.log(numberOfColoredFruits("blue")); 
+// console.log(numberOfColoredFruits("pink")); 
+// console.log(numberOfCategoryFruits("citrus")); 
+// console.log(numberOfCategoryFruits("berry")); 
+// console.log(numberOfCategoryFruits("unknown"));
+// console.log(getFruitsByCategory("berry")); 
+// console.log(getFruitsByCategory("unknown"));
+// console.log(getFruitsByColor("red")); 
+// console.log(getFruitsByColor("purple")); 
+// console.log(getFruitsByColor("pink"));
+// console.log(getIconsByCategory("berry")); 
+// console.log(getIconsByCategory("unknown"));
+// console.log(getIconsByColor("yellow")); 
+// console.log(getIconsByColor("green")); 
+// console.log(getIconsByColor("unknown")); 
+// console.log(getColors()); 
+// console.log(getFruit("")); 
+// console.log(getIconsByCategory("")); 
+
+
+
+
